4bc19a481d133d4e6c508c5317bc01dc9359c76c
Wojciech Schabowki <swojciech35@gmail.com>
2024-06-12 21:19:06 +0200

    45 loader

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,47 +10,9 @@ import {ToastrService} from "ngx-toastr";
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-  // signupForm: FormGroup;
-  // hidePassword: boolean = true;
-  //
-  // constructor(private formBuilder: FormBuilder) {
-  //   this.signupForm = this.formBuilder.group({
-  //     email: ['', [Validators.required, Validators.email]], // Dodaj walidator dla pola email
-  //     password: ['', [Validators.required, Validators.minLength(6)]],
-  //     confirmPassword: ['', Validators.required]
-  //   }, { validators: this.passwordMatchValidator });
-  // }
-  //
-  // passwordMatchValidator(control: AbstractControl) {
-  //   const passwordControl = control.get('password');
-  //   const confirmPasswordControl = control.get('confirmPassword');
-  //
-  //   if (!passwordControl || !confirmPasswordControl) {
-  //     return null;
-  //   }
-  //
-  //   const password: string = passwordControl.value;
-  //   const confirmPassword: string = confirmPasswordControl.value;
-  //
-  //   if (password !== confirmPassword) {
-  //     confirmPasswordControl.setErrors({ passwordMismatch: true });
-  //     return { passwordMismatch: true };
-  //   } else {
-  //     confirmPasswordControl.setErrors(null);
-  //     return null;
-  //   }
-  // }
-  //
-  // togglePasswordVisibility() {
-  //   this.hidePassword = !this.hidePassword;
-  // }
-  //
-  // onSubmit() {
-  //   // Do something with the form data
-  // }
-
   signupForm: FormGroup;
   hidePassword: boolean = true;
+  isLoading = false;
 
   constructor(private formBuilder: FormBuilder, public authService: AuthService, private router: Router, private toastr: ToastrService) {
     this.signupForm = this.formBuilder.group({
@@ -86,16 +48,21 @@ export class SignupComponent {
 
   onSubmit() {
     if (this.signupForm.valid) {
+      this.isLoading = true;
       const formData = this.signupForm.value;
       this.authService.signup({email: formData.email, password: formData.password})
         .subscribe(result => {
           if (result == 200) {
+            this.isLoading = false;
             this.toastr.success('Rejestracja przebiegła pomyślnie')
             this.router.navigate(['/signin'])
+
           } else {
+            this.isLoading = false;
             this.toastr.error("Błąd podczas rejestracji")
           }
         }, error => {
+          this.isLoading = false;
           console.error('Błąd podczas rejestracji: ', error.message);
           this.toastr.error("Błąd podczas rejestracji")
         })
