93984eac6566f323a682079687b3b785f24dc91a
Wojciech Schabowki <swojciech35@gmail.com>
2024-06-14 17:32:28 +0200

    50 zmiana sampli na końcówki api

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,31 +1,36 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {RecipeService} from "../recipe.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.css'
 })
-export class RecipeDetailsComponent implements OnInit{
+export class RecipeDetailsComponent implements OnInit {
   public id: string = '';
-  recipe:any;
+  recipe: any = [];
+  isLoading = true
+
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: any) => {
       this.id = params.get('id');
-      this.recipeService.getRecipe(this.id).subscribe({
-        next: (recipe: any) => {
-          this.recipe = recipe;
-        },
-        error: (error: any) => {
-          console.error('Error fetching recipe:', error);
-        }
-      });
-    });
-    console.log(this.recipe)
+    })
+    this.recipeService.getRecipe(this.id).subscribe(result => {
+      if (result.status == 200) {
+        this.isLoading = false;
+        this.recipe = result.body
+      } else {
+        this.toastr.error("Wystąpił błąd podczas pobierania danych")
+      }
+    }, error => {
+      console.error('Wystąpił błąd podczas pobierania danych: ', error.message);
+      this.toastr.error("Wystąpił błąd podczas pobierania danych")
+      this.ngOnInit()
+    })
   }
 
-
-  constructor(private recipeService: RecipeService,private route: ActivatedRoute) {
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private toastr: ToastrService) {
   }
 }
